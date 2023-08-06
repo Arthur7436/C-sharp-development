29afa0736162afe48e6658c9a246fe0efe9fdda4
arthur <arthur.thai479@gmail.com>
2023-08-06 22:42:56 +1000

    update

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,6 +8,8 @@ namespace Maths
         {
             //Console.Write allows you to stay on the same line in the terminal as compared to Console.WriteLine which 
             //places user in the next line like /n
+
+            //Ask the user for the first number
             Console.Write("Input the first number: ");
             double firstNum = double.Parse(Console.ReadLine());
 
