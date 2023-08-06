2ea0a523f973d0a387669df0124db929b26fd199
arthur <arthur.thai479@gmail.com>
2023-08-06 22:25:25 +1000

    update

---
+++ Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Maths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the first number: ");
+            int firstNum = int.Parse(Console.ReadLine());
+            
+            Console.Write("Input the second number: ");
+            int secondNum = int.Parse(Console.ReadLine());
+
+            Console.Write("Do you want to + - * /? ");
+            string operation = Console.ReadLine();
+            
+            
+
+        
+        }
+    }
+}
