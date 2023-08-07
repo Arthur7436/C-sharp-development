f755f9463840f46eb130d3916c9c54d7a4e65980
arthur <arthur.thai479@gmail.com>
2023-08-07 11:39:00 +1000

    update

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,45 +6,64 @@ namespace Maths
     {
         static void Main(string[] args)
         {
-            //Console.Write allows you to stay on the same line in the terminal as compared to Console.WriteLine which 
-            //places user in the next line like /n
-
-            //Ask the user for the first number
-            //Double allows the program to use decimals
-            Console.Write("Input the first number: ");
-            double firstNum = double.Parse(Console.ReadLine());
-
-             //Ask the user for the second number
-            Console.Write("Input the second number: ");
-            double secondNum = double.Parse(Console.ReadLine());
-
-            //Ask the user which operator they would want to user
-            Console.Write("Do you want to + - * /? ");
-            string operation = Console.ReadLine();
-
-            //If else statements will determine the answers
-            if (operation == "+")
+            while (true)
             {
-                Console.WriteLine("Answer: " + (firstNum + secondNum));
-            }
-            else if (operation == "-")
-            {
-                Console.WriteLine("Answer: " + (firstNum - secondNum));
-            }
-            else if (operation == "*")
-            {
-                Console.WriteLine("Answer: " + (firstNum * secondNum));
-            }
-            else if (operation == "/")
-            {
-                Console.WriteLine("Answer: " + (firstNum / secondNum));
-            }
-            else {
-                Console.WriteLine("Error please try again");
-            }
+                //Console.Write allows you to stay on the same line in the terminal as compared to Console.WriteLine which 
+                //places user in the next line like /n
+
+                //Ask the user for the first number
+                Console.Write("Input the first number: ");
+                //If the user inputs "q", then the program breaks
+                string firstNumInput = Console.ReadLine();
+                if (firstNumInput == "q")
+                {
+                    break;
+                }
+                else
+                {
+                    double firstNum = double.Parse(firstNumInput);
+
+                }
+                //Ask the user for the second number
+                Console.Write("Input the second number: ");
+                //If the user inputs "q", then the program breaks
+                double secondNum = double.Parse(Console.ReadLine());
+
+                //Ask the user which operator they would want to user
+                Console.Write("Do you want to + - * /? ");
+                string operation = Console.ReadLine();
+
+                //If else statements will determine the answers
+                if (operation == "+")
+                {
+                    Console.WriteLine("Answer: " + (firstNum + secondNum).ToString("F2"));
+                }
+                else if (operation == "-")
+                {
+                    Console.WriteLine("Answer: " + (firstNum - secondNum).ToString("F2"));
+                }
+                else if (operation == "*")
+                {
+                    Console.WriteLine("Answer: " + (firstNum * secondNum).ToString("F2"));
+                }
+                else if (operation == "/")
+                {
+                    if (secondNum != 0)
+                    {
+                        Console.WriteLine("Answer: " + (firstNum / secondNum).ToString("F2"));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Cannot be divided by the value 0");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error please try again");
+                }
 
 
-
+            }
         }
     }
 }
