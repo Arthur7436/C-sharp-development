033e66abd2737ed778151d6821e669d625739469
arthur <arthur.thai479@gmail.com>
2023-08-07 21:09:10 +1000

    update

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -6,7 +6,18 @@ namespace ToDoList
     {
         static void Main(string[] args)
         {
-            
+            string[] options = { "Option 1", "Options 2", "Options 3" , "Exit"};
+            do
+            {
+                for (int i = 0; i < options.Length; i++)
+                {
+                    Console.WriteLine(options[i]);
+                }
+
+            } while (true);
+
+
         }
     }
+
 }
