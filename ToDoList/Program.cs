a37cb4f733292dac295b25e9b12d035aa9e6cf3d
arthur <arthur.thai479@gmail.com>
2023-08-07 21:31:40 +1000

    update

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -6,18 +6,27 @@ namespace ToDoList
     {
         static void Main(string[] args)
         {
-            string[] options = { "Option 1", "Options 2", "Options 3" , "Exit"};
+            string[] options = { "Option 1", "Option 2", "Option 3", "Exit" };
+            int selectedIndex = 0;
+            ConsoleKeyInfo keyInfo;
+
             do
             {
+                Console.Clear();
+                Console.WriteLine("Use arrow keys to navigate through menu");
+
                 for (int i = 0; i < options.Length; i++)
                 {
-                    Console.WriteLine(options[i]);
+                    if (i == selectedIndex)
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkBlue;
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    
                 }
 
-            } while (true);
-
-
+            } while(true);
         }
     }
-
 }
+
