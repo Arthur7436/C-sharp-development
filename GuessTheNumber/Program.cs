08c139f952fe8635427f42ea158a5df7086486a3
arthur <arthur.thai479@gmail.com>
2023-08-21 19:55:22 +1000

    update

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -6,10 +6,31 @@ namespace GuessTheNumber
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+
+            int randomNumber = random.Next(1, 10);
+
             do
             {
-                Console.WriteLine("HelloWorld");
-                Console.ReadLine();
+
+                Console.Clear();
+                Console.WriteLine("Welcome to 'Guess The Number' game!");
+                Console.WriteLine("Please choose a number between 1 - 10");
+                Console.Write("Insert number: ");
+
+                int num = int.Parse(Console.ReadLine());
+
+                if (num == randomNumber)
+                {
+                    Console.WriteLine("You got it!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect! Press Enter to try again");
+                    Console.ReadLine();
+
+                }
 
 
             }
