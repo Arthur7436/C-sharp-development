22f3556c6c96a2ab03c8c04ef6d8042b26b384cf
arthur <arthur.thai479@gmail.com>
2023-08-21 20:12:23 +1000

    change folder name

---
+++ MethodCallingBasics/Class.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ClassLibrary
+{
+    class OtherClass
+    {
+        public static string GetName()
+        {
+            return "David";
+        }
+    }
+}
--- MethodCallingBasics/Program.cs
+++ MethodCallingBasics/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ClassLibrary;
 
 namespace WeatherApp
 {
@@ -6,7 +7,8 @@ namespace WeatherApp
     {
         static void Main(string[] args)
         {
-
+            string name = OtherClass.GetName();
+            Console.WriteLine($"Hello {name}");
         }
     }
 }
