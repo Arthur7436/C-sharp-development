1179284abad90936b01d5d6bf124583baa173a71
arthur <arthur.thai479@gmail.com>
2023-09-02 21:00:06 +1000

    update

---
+++ Survey/Program.cs
@@ -0,0 +1,12 @@
+﻿using System; // 2. Importing Libraries
+
+namespace HelloWorld // 1. Namespace Declaration
+{
+    class Program // 3. Class Definition
+    {
+        static void Main(string[] args) // 4. Main Method
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
