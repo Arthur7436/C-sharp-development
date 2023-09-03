ea5e724aa650ef48636520a43285d8b8c27d9c04
arthur <arthur.thai479@gmail.com>
2023-09-03 22:06:05 +1000

    update

---
+++ Survey/DataModel.cs
@@ -0,0 +1,13 @@
+﻿using System; // 2. Importing Libraries
+
+namespace HelloWorld // 1. Namespace Declaration
+{
+    public class FormData // 3. Class Definition
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string country { get; set; }
+        public int age { get; set; }
+    }
+}
