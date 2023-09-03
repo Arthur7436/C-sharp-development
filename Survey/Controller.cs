1e8e4ae15ae53066c4ba6acdbe484979239ea13f
arthur <arthur.thai479@gmail.com>
2023-09-03 22:31:42 +1000

    mistake

---
+++ Survey/Controller.cs
@@ -0,0 +1,18 @@
+﻿public class SurveyController : Controller
+{
+    public IActionResult DisplayForm()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult HandleFormSubmission(FormData formData)
+    {
+        if (ModelState.IsValid)
+        {
+            // Process form data
+            return RedirectToAction("Success");
+        }
+        return View("DisplayForm");
+    }
+}
