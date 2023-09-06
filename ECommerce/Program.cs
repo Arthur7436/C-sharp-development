977c524d2befbabaa886ee29f2c31c9786832898
arthur <arthur.thai479@gmail.com>
2023-09-06 21:58:36 +1000

    update

--- ECommerce/Program.cs
+++ ECommerce/Program.cs
@@ -6,7 +6,7 @@ namespace ECommercePlatform
     {
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
---
+++ ECommerce/Roles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ECommercePlatform
+{
+    enum UserRole
+    {
+
+    }
+
+    enum ProductCategory
+    {
+
+    }
+
+    enum OrderStatus
+    {
+
+    }
+
+    class Seller
+    {
+        public string ID { get; set; }
+        public string name { get; set; }
+
+    }
+}
