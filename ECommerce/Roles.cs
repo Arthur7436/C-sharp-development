f35019eb88a1f0d86c23e47264cbd65d9ecd263f
Arthur7436 <arthur.thai479@gmail.com>
2023-09-07 08:23:14 +1000

    update

--- ECommerce/Roles.cs
+++ ECommerce/Roles.cs
@@ -1,26 +1,57 @@
 ﻿using System;
+using System.Dynamic;
 
-namespace ECommercePlatform
+namespace ECommercePlatform.Roles
 {
     enum UserRole
     {
-
+        Admin,
+        Buyer,
+        Seller
     }
 
     enum ProductCategory
     {
-
+        Digital,
+        HouseholdItems,
+        Utilities,
+        KitchenWare
     }
 
     enum OrderStatus
     {
+        PartiallyPaid,
+        Paid,
+        Returned,
+        Closed
+    }
+
+    class Admin
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string country { get; set; }
+
+    }
+    class Seller
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string country { get; set; }
 
     }
 
-    class Seller
+    class Buyer
     {
-        public string ID { get; set; }
-        public string name { get; set; }
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string country { get; set; }
 
     }
 }
