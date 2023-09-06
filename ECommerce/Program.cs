fae5aaae7266e6a740bd7f070d4dc1da0103762d
arthur <arthur.thai479@gmail.com>
2023-09-06 21:26:02 +1000

    update

--- ECommerce/Program.cs
+++ ECommerce/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ECommercePlatform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the E-Commerce Platform!");
+            // Your code goes here...
+        }
+    }
+}
