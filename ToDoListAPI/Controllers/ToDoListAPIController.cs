babf42cc35f25ffa391bda231bf87982adf988bb
Arthur7436 <arthur.thai479@gmail.com>
2023-09-08 20:21:33 +1000

    update

--- ToDoListAPI/Controllers/ToDoListAPIController.cs
+++ ToDoListAPI/Controllers/ToDoListAPIController.cs
@@ -19,41 +19,12 @@ namespace ToDoListAPI.Controllers
         public ViewController(ILogger<ViewController> logger)
         {
             _logger = logger;
+
         }
 
         [HttpGet(Name = "GetTaskAPI")]
         public IActionResult Get()
         {
-            Task instance1 = new Task();
-            {
-                instance1.Id = 21;
-                instance1.Description = "This is the first instance";
-                instance1.IsCompleted = false;
-            }
-
-            Task instance2 = new Task();
-            {
-                instance2.Id = 100;
-                instance2.Description = "This is the second instance";
-                instance2.IsCompleted = true;
-            }
-
-            Task instance3 = new Task();
-            {
-                instance3.Id = 1;
-                instance3.Description = "This is the third instance";
-                instance3.IsCompleted = false;
-            }
-
-            taskList.Add(instance1);
-            taskList.Add(instance2);
-            taskList.Add(instance3);
-
-            if (taskList == null || taskList.Count == 0)
-            {
-                return NotFound("No list has been found");
-            }
-
             return Ok(taskList);
         }
 
@@ -63,12 +34,9 @@ namespace ToDoListAPI.Controllers
         {
             taskList.Add(newTask);
 
-
             return Ok(newTask);
 
 
         }
     }
-
-
 }
