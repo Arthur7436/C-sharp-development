e21dac31c8c01827b2383d201ffa2df355a2b065
Arthur7436 <arthur.thai479@gmail.com>
2023-09-08 10:34:02 +1000

    update

--- MiniAPI/ToDoAPI.cs
+++ MiniAPI/ToDoAPI.cs
@@ -2,13 +2,5 @@ namespace MiniAPI;
 
 public class Task
 {
-    public string Date { get; set; }
 
-    public int TemperatureC { get; set; }
-
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-    public string? Summary { get; set; }
-    public int Age { get; set; }
-    public string RandomProperty { get; set; }
 }
