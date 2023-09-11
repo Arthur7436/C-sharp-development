b78bfdf516a5e09ce723114ad74caad7acb6e1ac
Arthur7436 <arthur.thai479@gmail.com>
2023-09-11 21:17:28 +1000

    update

---
+++ ECommerce/UserChoice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ECommercePlatform.Roles;
+using People;
+
+namespace UserChoice
+{
+    public static class Choices
+    {
+        public static void AdminChoice()
+        {
+            Admin admin = UserCreation.CreateAdmin();
+            Console.WriteLine(admin.ToString());
+            Console.ReadLine();
+        }
+
+        public static void BuyerChoice()
+        {
+            Admin buyer = UserCreation.CreateAdmin();
+            Console.WriteLine(buyer.ToString());
+            Console.ReadLine();
+        }
+
+        // public static void SellerChoice()
+        // {
+        //     Admin seller = UserCreation.CreateAdmin();
+        //     Console.WriteLine(seller.ToString());
+        //     Console.ReadLine();
+        // }
+
+    }
+}
