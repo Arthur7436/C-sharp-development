397339720fc484cb440f56c763a20800b1b2d224
Arthur7436 <arthur.thai479@gmail.com>
2023-09-12 15:12:00 +1000

    update

---
+++ AnimalAbstractConcrete/Class.cs
@@ -0,0 +1,27 @@
+﻿namespace AnimalAbstractConcrete.AnimalClass
+{
+    public abstract class AnimalClass
+    {
+        public abstract string Speak();
+        public abstract string Name();
+        public abstract string Eat();
+    }
+
+    public class Lion : AnimalClass
+    {
+        public override string Speak()
+        {
+            return "This is a Lion... ROAR!!";
+        }
+
+        public override string Name()
+        {
+            return "The Lion's name is LEO!!";
+        }
+
+        public override string Eat()
+        {
+            return "The lion is eating now...";
+        }
+    }
+}
--- AnimalAbstractConcrete/Program.cs
+++ AnimalAbstractConcrete/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace AnimalAbstractConcrete
+﻿using AnimalAbstractConcrete.AnimalClass;
+
+
+namespace AnimalAbstractConcrete
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            Lion leo = new Lion();
+            Console.WriteLine(leo.Name());
+            Console.WriteLine(leo.Speak());
+            Console.WriteLine(leo.Eat());
         }
     }
 }
