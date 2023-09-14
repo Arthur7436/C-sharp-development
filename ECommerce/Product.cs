1d9fc2ec3d7a44519da93ad6514710eb39389ae5
Arthur7436 <arthur.thai479@gmail.com>
2023-09-14 14:33:45 +1000

    update

--- ECommerce/Product.cs
+++ ECommerce/Product.cs
@@ -1,23 +1,32 @@
-﻿using ECommerce.Interface.IProduct;
-
-namespace ECommerce.Product
+﻿namespace ECommerce.Product
 {
-    public class Product : IProduct
+    //public class Product : IProduct
+    //{
+    //    public string GetId() //returns a random id for the product
+    //    {
+    //        return Guid.NewGuid().ToString("N");
+
+    //    }
+
+    //    public string GetNameOfProduct()
+    //    {
+
+    //        return "Test";
+    //    }
+
+    //    public string GetDescriptionOfProduct()
+    //    {
+    //        return "Test";
+    //    }
+    //}
+
+    public class Product
     {
-        public string GetId() //returns a random id for the product
-        {
-            return Guid.NewGuid().ToString("N");
-
-        }
-
-        public string GetNameOfProduct()
-        {
-            return "Test";
-        }
-
-        public string GetDescriptionOfProduct()
-        {
-            return "Test";
-        }
+        public string Id { get; set; }
+        public string NameOfProduct { get; set; }
+        public string Description { get; set; }
     }
+
+
+
 }
--- ECommerce/Program.cs
+++ ECommerce/Program.cs
@@ -6,7 +6,8 @@ namespace ECommercePlatform
     {
         static void Main(string[] args)
         {
-            List<string> ListOfProducts = new List<string>();
+            List<Product> ListOfProducts = new List<Product>();
+
             do
             {
                 Console.Clear();
@@ -45,19 +46,50 @@ namespace ECommercePlatform
                 if (input == "2")
                 {
                     Product product = new Product();
+
+                    string GenerateRandomID = Guid.NewGuid().ToString("N");
+
+                    Console.WriteLine($"Product Id: {GenerateRandomID}");
+                    product.Id = GenerateRandomID;
+
+                    Console.WriteLine("Name of product: ");
+                    string? ProductNameInput = Console.ReadLine();
+                    product.NameOfProduct = ProductNameInput!;
+
+                    Console.WriteLine("Description of product: ");
+                    string? DescriptionOfProductInput = Console.ReadLine();
+                    product.Description = DescriptionOfProductInput!;
+
+                    ListOfProducts.Add(product);
+
+                    Console.WriteLine(product);
+
+                    Console.WriteLine(ListOfProducts[0].ToString());
+
+                    Console.WriteLine(ListOfProducts[0]);
+
+                    Console.WriteLine(ListOfProducts.ToArray());
+
+
+
+
+
                     //add id for product
-                    Console.Write("Product Id: ");
-                    Console.WriteLine(product.GetId());
+                    //Console.Write("Product Id: ");
+                    //Console.WriteLine(product.GetId());
+
+
 
                     //add name of product
-                    Console.WriteLine("Product name: ");
-                    string? ProductName = Console.ReadLine();
-                    product.GetNameOfProduct();
+                    //Console.Write("Product name: ");
+                    //string? ProductName = Console.ReadLine();
+
+                    //product.GetNameOfProduct();
 
                     //Description of product
-                    Console.WriteLine("Description of product: ");
-                    string? DescriptionOfProduct = Console.ReadLine();
-                    product.GetDescriptionOfProduct();
+                    //Console.Write("Description of product: ");
+                    //string? DescriptionOfProduct = Console.ReadLine();
+                    //product.GetDescriptionOfProduct();
 
                     Console.ReadLine();
                 }
