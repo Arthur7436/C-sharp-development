5b8d449e09614b539864edbd6368555298bc169d
Arthur7436 <arthur.thai479@gmail.com>
2023-09-15 11:22:42 +1000

    update

--- ECommerce/Product.cs
+++ ECommerce/Product.cs
@@ -1,25 +1,5 @@
 ﻿namespace ECommerce.Product
 {
-    //public class Product : IProduct
-    //{
-    //    public string GetId() //returns a random id for the product
-    //    {
-    //        return Guid.NewGuid().ToString("N");
-
-    //    }
-
-    //    public string GetNameOfProduct()
-    //    {
-
-    //        return "Test";
-    //    }
-
-    //    public string GetDescriptionOfProduct()
-    //    {
-    //        return "Test";
-    //    }
-    //}
-
     public class Product
     {
         public string Id { get; set; }
