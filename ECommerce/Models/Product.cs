f6885b6d87793e21a1593accc7f877c8792e434f
Arthur7436 <arthur.thai479@gmail.com>
2023-09-19 11:58:37 +1000

    refactor code

--- ECommerce/Models/Product.cs
+++ ECommerce/Models/Product.cs
@@ -1,11 +1,15 @@
 ﻿using ECommerce.Interface.IProductOperations;
+using System.ComponentModel.DataAnnotations;
 
-namespace ECommerce
+namespace ECommerce.Models
 {
     public class Product : IProduct
     {
+        [ScaffoldColumn(false)]
         public string? Id { get; set; }
+        [Required, StringLength(100), Display(Name = "Name")]
         public string? NameOfProduct { get; set; }
+        [Required, StringLength(100), Display(Name = "Product Description"), DataType(DataType.MultilineText)]
         public string? Description { get; set; }
         public override string ToString() //convert the product into readable string
         {
