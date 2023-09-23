58aa702e7a873726cba6d349a0a45ae24ad10185
Arthur7436 <arthur.thai479@gmail.com>
2023-09-23 17:23:06 +1000

    refactor code for web API

--- ECommerceAPI/Controllers/ECommerceController.cs
+++ ECommerceAPI/Controllers/ECommerceController.cs
@@ -1,4 +1,6 @@
+using ECommerce.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 
 namespace ECommerceAPI.Controllers
 {
@@ -6,8 +8,6 @@ namespace ECommerceAPI.Controllers
     [Route("[controller]")]
     public class ECommerceController : ControllerBase
     {
-
-
         private readonly ILogger<ECommerceController> _logger;
 
         public ECommerceController(ILogger<ECommerceController> logger)
@@ -16,10 +16,29 @@ namespace ECommerceAPI.Controllers
         }
 
         [HttpGet(Name = "GetECommerce")]
-        public IEnumerable<E> Get()
+        public IEnumerable<Product> Get()
         {
+            string pwd = Environment.GetEnvironmentVariable("SQL_PASSWORD", EnvironmentVariableTarget.Machine)!; //used SETX command to store SQL_PASSWORD into local machine so that credentials are not hard-coded
 
+            _logger.LogInformation("Storage of password in variable was successful...");
 
+            //Attempt to connect console application to server database
+
+            //variable declaration
+            string connectionString = null!;
+            SqlConnection cnn;
+            connectionString = $"Data Source=AUL0953;Initial Catalog=ProductDB;User ID=sa;Password={pwd}";
+
+            cnn = new SqlConnection(connectionString);
+
+            try
+            {
+                cnn.Open();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation($"{ex.Message}");
+            }
 
         }
     }
